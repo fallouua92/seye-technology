aaf7b624eee67df8e9c5307cacadb1a4987721dd
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-11-29 11:14:02 +0000

    Update form.php

--- form.php
+++ form.php
@@ -10,7 +10,7 @@
             .form_name{  
                 padding: 10px;
             }
-            .form_address{  
+            .form_address{ 
                 padding: 10px;
             }
             .form_email{  
@@ -24,7 +24,7 @@
     <body>
         <h1>My Form</h1>
         <form action="demo_backend.php" method="POST">
-        <div class= "form_name">First Name: <input type="text" name="name"><br></div>
+        <div class= "form_name">Name: <input type="text" name="name"><br></div>
         <div class= "form_email">E-mail: <input type="text" name="email"><br></div>
         <div class= "form_address">Address: <input type="text" name="address"><br></div>
          <div class= "sub_button"><input type="submit"></div>
