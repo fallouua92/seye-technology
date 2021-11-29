55e899600af11ee67b84e87f0fbb375b7f696f30
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-11-29 10:24:12 +0000

    form files added

---
+++ demo_backend.php
@@ -0,0 +1,36 @@
+<?php 
+define("DB_NAME", "ua92backend");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_HOST", "localhost");
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$connection) {
+    die('Could not connect:'. mysqli_error($connection));
+}
+else {
+    echo "It worked!";
+}
+
+$db_selected = mysqli_select_db($connection, DB_NAME);
+
+if (!$db_selected){
+    die('Could not connect:'.mysqli_error($connection));
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$address = $_POST["address"];
+
+$sql = "INSERT INTO details(name, Email, Address) VALUES ('$name', '$email', '$address')";
+if(!mysqli_query($connection, $sql)){
+    die("Error:".mysqli_error($connection));
+}
+else{
+    echo "Data Inserted";
+}
+
+mysqli_close($connection);
+?>
+
---
+++ form.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+    <head>
+        <title>My PHP Form</title>
+        <style>
+            body {
+                background-color: lightgrey;
+                text-align: center;
+            }
+            .form_name{  
+                padding: 10px;
+            }
+            .form_address{  
+                padding: 10px;
+            }
+            .form_email{  
+                padding: 10px;
+            }
+            .sub_button{  
+                padding: 
+            }
+        </style>
+    </head>
+    <body>
+        <h1>My Form</h1>
+        <form action="demo_backend.php" method="POST">
+        <div class= "form_name">Name: <input type="text" name="name"><br></div>
+        <div class= "form_email">E-mail: <input type="text" name="email"><br></div>
+        <div class= "form_address">Address: <input type="text" name="address"><br></div>
+         <div class= "sub_button"><input type="submit"></div>
+        </form>
+    </body>
+</html> 
