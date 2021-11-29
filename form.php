bde4873d4599ef74205f624034da906bc139603e
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-11-29 10:36:05 +0000

    minor change

--- form.php
+++ form.php
@@ -25,7 +25,7 @@
         <h1>My Form</h1>
         <form action="demo_backend.php" method="POST">
         <div class= "form_name">Name: <input type="text" name="name"><br></div>
-        <div class= "form_email">E-mail: <input type="text" name="email"><br></div>
+        <div class= "form_email">E mail: <input type="text" name="email"><br></div>
         <div class= "form_address">Address: <input type="text" name="address"><br></div>
          <div class= "sub_button"><input type="submit"></div>
         </form>
