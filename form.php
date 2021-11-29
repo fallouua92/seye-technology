961cab9b2c870a6087f13b5877928b343e072b69
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-11-29 12:07:31 +0000

    update

--- form.php
+++ form.php
@@ -24,11 +24,7 @@
     <body>
         <h1>My Form to apply personal details</h1>
         <form action="demo_backend.php" method="POST">
-<<<<<<< HEAD
-        <div class= "form_name">First Name:: <input type="text" name="name"><br></div>
-=======
-        <div class= "form_name">Name: <input type="text" name="name"><br></div>
->>>>>>> 403a63e9114df7d84b6479521f02c0240b78c566
+        <div class= "form_name">First Name: <input type="text" name="name"><br></div>
         <div class= "form_email">E-mail: <input type="text" name="email"><br></div>
         <div class= "form_address">Address: <input type="text" name="address"><br></div>
          <div class= "sub_button"><input type="submit"></div>
