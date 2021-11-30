edd8192ab2ff7b4e964650492232c8aca6401265
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-11-30 12:21:17 +0000

    update

--- mission_form.php
+++ mission_form.php
@@ -23,27 +23,50 @@
     </head>
     <body>
 
-<!-- This the div with class row and column for the header -->
-<div class="row">
-        <div class="col d-flex justify-content-center bg-dark text-white">
-            <h1>Mission form</h1>
-        </div>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Pet Website</title>
+    <!--This is my bootstrap with CSS link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--This is my JavaScript link-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </head>
+<body>
+    <!--I have used the div here with class row and column for the header of the page-->
+    <div class="alert alert-info">
+      <strong>Let us know you agree to cookies!</strong> We use cookies to improve your experience on our Website. 
     </div>
+    
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <h1>Pet Website</h1>
+    </div>
+    <div>
+        <!--another row and column for the navbar-->
     <div class="row">
         <div class="col-lg">
             <ul class="nav nav-tabs justify-content-center">
                 <li class="nav-item">
-                  <a class="nav-link" href="index.html">Home Page</a>
+                  <a class="nav-link active" href="index.html">Home Page</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link active" href="astronaut_form.php">Astronaut form</a>
+                  <a class="nav-link" href="dogs.html">dogs</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="attends_form.php">Attends form</a>
+                    <a class="nav-link" href="cats.html">Cats</a>
+                  </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="pets.html">Pets Products</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="targets_form.php">Targets form</a>
+                  <a class="nav-link" href="shoppingcart/cart.html">Shopping Cart</a>
                 </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="contact.html">Contact Us</a>
+                </li>
+                
               </ul>
         </div>
 
