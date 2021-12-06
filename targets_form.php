fff36460edd84a705289de2e68ffb72ff8d3b206
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-06 12:57:30 +0000

    update

--- targets_form.php
+++ targets_form.php
@@ -48,7 +48,7 @@ if ($submitbutton){
     <body>
        <!--Bootstrap has been used here with CSS link-->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!--This is my JavaScript link-->
+    <!--This is a JavaScript link-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </head>
