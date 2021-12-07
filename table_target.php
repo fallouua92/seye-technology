cee6e85e54b5b7a74e7b56f96860a897135e5544
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-07 12:31:01 +0000

    up

---
+++ table_target.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>This is the database table</title>
+</head>
+<body>
+    
+</body>
+    <table> 
+<th> 
+        <th>id</th>
+        <th>name</th>
+        <th>first_mission</th>
+        <th>type</th>
+        <th>no_mission</th>
+</th> 
+<?php
+$conn = mysqli_connect("localhost","admin","password", "Fama2022#");
+if ($conn ->connect_error) {
+    die("connection faild:". $conn-> connect_error);
+}
+
+$sql = "SELECT * FROM targest";
+$result = mysqli_query($conn, $sql);
+
+if (mysql_num_rows($result) > 0){
+
+while ($row = mysql_fetch_assoc($result)){
+    echo "<tr><td>" . $row["id"] . "</td><td>". $row["name"] . "<td><td>" . $row[ "first_mission"] . "</td><td>" . $row["type"] . "</td><td>" . $row["no_mission"] . "</td></td>";
+}    
+
+
+?>
+    </table>
+
+</html>
