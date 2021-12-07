388fc50df3fbb1d825034e07a17d88f695828dff
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-07 15:52:40 +0000

    update

---
+++ table_mission.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>This is the database table</title>
+</head>
+<body>
+
+    <table> 
+    
+<tr> 
+        <th>mission_id</th>
+        <th>Name</th>
+        <th>Destination</th>
+        <th>launch_date</th>
+        <th>Type</th>
+        <th>Crew_size</th>
+        <th>Target_id</th>
+</tr> 
+<?php
+$conn = mysqli_connect("localhost","fallou","Fama2022#", "ua92backend");
+if ($conn ->connect_error) {
+    die("connection faild:". $conn-> connect_error);
+}
+
+$sql = "SELECT * FROM mission";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0){
+while ($row = mysqli_fetch_assoc($result)){
+    echo "<tr><td>" . $row["mission_id"] . "</td><td>". $row["Name"] . "</td><td>" . $row[ "Destination"] . "</td><td>" . $row["Launch_date"] . "</td><td>" . $row["Type"] . "</td></tr>" $row["Crew_size"] . "</td></tr>" $row["Target_id"];
+}
+}
+else{
+    echo "0 results";
+}    
