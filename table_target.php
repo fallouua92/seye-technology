e4c8e9af820baad964224b1418ebdcabe23a3920
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-07 12:47:20 +0000

    up

--- table_target.php
+++ table_target.php
@@ -8,13 +8,14 @@
     
 </body>
     <table> 
-<th> 
+    
+<tr> 
         <th>id</th>
         <th>name</th>
         <th>first_mission</th>
         <th>type</th>
         <th>no_mission</th>
-</th> 
+</tr> 
 <?php
 $conn = mysqli_connect("localhost","admin","Fama2022#", "ua92backend");
 if ($conn ->connect_error) {
@@ -24,7 +25,7 @@ if ($conn ->connect_error) {
 $sql = "SELECT * FROM targets";
 $result = mysqli_query($conn, $sql);
 
-if (mysql_num_rows($result) > 0){
+if (mysqli_num_rows($result) > 0){
 while ($row = mysqli_fetch_assoc($result)){
     echo "<tr><td>" . $row["id"] . "</td><td>". $row["Name"] . "</td><td>" . $row[ "first_mission"] . "</td><td>" . $row["type"] . "</td><td>" . $row["no_missions"] . "</td></tr>";
 }
