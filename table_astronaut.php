e63830a0e01715611fdbd884ada263cc7515c5f1
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-07 15:42:08 +0000

    update

--- table_astronaut.php
+++ table_astronaut.php
@@ -1,25 +1,33 @@
+<!DOCTYPE html>
 <html>
-    <body> 
-        <table>
-            <tr>
-                <th>astronaut_id</th>
-                <th>Name</th>
-                <th>no_missions</th>
-            </tr>
-            <?php 
-            $conn = mysqli_connect("localhost", "fallou", "Fama2022#", "ua92backend");
-            $sql = "SELECT * FROM astronaut";
-            $result = $conn->query($sql);
-            if ($result->num_roms > 0) {
-                while ($row = $result-> fetch_assoc()) {
-                    echo "<tr><td>" . $row["astronaut_id"] . "</td><td>" .  $row["astronaut_id"] . "</td><td>" .  $row["Name"] . "</td><td>" .  $row["no_missions"] . "</td><td>";
-                }
-            } 
-            else {
-                echo"No result";
-            }
-            $conn->close();
-            ?>
-       </table>
-    </body>
-</html>   
+<head>
+    
+    <title>This is the database table</title>
+</head>
+<body>
+
+    <table> 
+    
+<tr> 
+        <th>astronaut_id</th>
+        <th>Name</th>
+        <th>no_missions</th>
+        
+</tr> 
+<?php
+$conn = mysqli_connect("localhost","fallou","Fama2022#", "ua92backend");
+if ($conn ->connect_error) {
+    die("connection faild:". $conn-> connect_error);
+}
+
+$sql = "SELECT * FROM astronaut";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0){
+while ($row = mysqli_fetch_assoc($result)){
+    echo "<tr><td>" . $row["astronaut_id"] . "</td><td>". $row["Name"] . "</td><td>" . $row[ "no_missiona"] . "</td><td>" ;
+}
+}
+else{
+    echo "0 results";
+}    
