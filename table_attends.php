9e5be80d3362609868bd2839e11fe9ca851e36e2
fallouua92 <91126667+fallouua92@users.noreply.github.com>
2021-12-07 16:02:45 +0000

    update

---
+++ table_attends.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>This is the database table</title>
+</head>
+<body>
+
+    <table> 
+    
+<tr> 
+        <th>mission_id</th>
+        <th>astronaut_id</th>
+        
+</tr> 
+<?php
+$conn = mysqli_connect("localhost","fallou","Fama2022#", "ua92backend");
+if ($conn ->connect_error) {
+    die("connection faild:". $conn-> connect_error);
+}
+
+$sql = "SELECT * FROM attends";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0){
+while ($row = mysqli_fetch_assoc($result)){
+    echo "<tr><td>" . $row["mission_id"] . "</td><td>". $row["astronaut_id"] . "</td><td>" ;
+}
+}
+else{
+    echo "0 results";
+}    
